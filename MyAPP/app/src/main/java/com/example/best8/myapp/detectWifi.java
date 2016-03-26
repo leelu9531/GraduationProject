b6b9cc530721b204a3a53a709bbc8ee3600fc74e
leelu9531 <best840301@gmail.com>
2016-03-26 17:12:22 +0800

    刪除測試檔案, 修補壞掉的MyAPP檔案

---
+++ MyAPP/app/src/main/java/com/example/best8/myapp/detectWifi.java
@@ -0,0 +1,13 @@
+package com.example.best8.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class detectWifi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detect_wifi);
+    }
+}
