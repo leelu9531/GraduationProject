1fa0f153fb6261934af4f24346a1cc8989328d0c
leelu9531 <best840301@gmail.com>
2016-05-11 14:27:09 +0800

    Revert "刪除多餘code  /"
    This reverts commit b0e0dbf666fe16c2928cc42d5a13626ffc53c235.

--- MyAPP/app/src/main/java/com/example/best8/myapp/MainActivity.java
+++ MyAPP/app/src/main/java/com/example/best8/myapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.best8.myapp;
 
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
 import android.net.wifi.ScanResult;
 import android.os.HandlerThread;
@@ -22,6 +25,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
@@ -41,26 +45,33 @@ public class MainActivity extends AppCompatActivity {
     private Handler mThreadHandler ;
     private HandlerThread mThread;
 
+    static private int counter = 0 ;
+private int temp=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        textStatus= (TextView) findViewById(R.id.textStatus);
-        listView  = (ListView) findViewById(R.id.wifiListView);
+        textStatus = (TextView) findViewById(R.id.textView2);
         startBtn  = (Button) findViewById(R.id.startBtn);
         stopBtn   = (Button) findViewById(R.id.stopBtn);
+        listView  = (ListView) findViewById(R.id.listView1);
 
         wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 
-
         mThread = new HandlerThread("Name");
+
         mThread.start();
         mThreadHandler = new Handler(mThread.getLooper());
 
         /*check wifi turn on*/
         checkWifiState();
 
+/*******************************************//*******************************************/
+        //this.adapter = new SimpleAdapter(MainActivity.this, arrayList, R.layout.list, new String[] {"detectTime","SSID","power","MAC"}, new int[] {R.id.detectTime,R.id.SSID, R.id.power, R.id.MAC});
+        //listView.setAdapter(adapter);
+/*******************************************//*******************************************/
 
         /*Start Btn clicked*/
         startBtn.setOnClickListener(new View.OnClickListener() {
@@ -69,11 +80,22 @@ public class MainActivity extends AppCompatActivity {
                 adapter = new SimpleAdapter(MainActivity.this, arrayList, R.layout.list, new String[] {"detectTime","SSID","power","MAC"}, new int[] {R.id.detectTime,R.id.SSID, R.id.power, R.id.MAC});
                 listView.setAdapter(adapter);
 
+/*
+                registerReceiver(new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        resultList = wifi.getScanResults();
+                        Log.v("HEYHU", "result list");
+                        size = resultList.size();
+                        Log.v("HEYHU", "result list size" + resultList.size());
+                    }
+                }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+*/
+
                 mThreadHandler.post(detectWifiLoop);
             }
         });
 
-        /*Stop Btn clicked*/
         stopBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 //解聘工人 (關閉Thread)
+
                 if (mThread != null) {
                     mThread.quit();
                 }
@@ -100,18 +123,30 @@ public class MainActivity extends AppCompatActivity {
 
         public void run(){
 
+
+            //Log.v("HEYHU", "in run!!!!!!");
+
+
             allWifiList.clear();
 
+            //Log.v("HEYHU", "before scan");
             wifi.startScan();
             resultList = wifi.getScanResults();
             size = resultList.size();
+            //Log.v("HEYHU", "after  scan");
+
+
 
             //取得現在系統時間
             Time nowTime = new Time("GMT+8");
             nowTime.setToNow();
 
+
+
             try {
                 size = size - 1;
+                temp = size;
+                Log.v("HEYHU", "scan result (resultList)"+ resultList.size());
                 while(size >= 0 ){
                     HashMap item = new HashMap();
 
@@ -121,13 +156,25 @@ public class MainActivity extends AppCompatActivity {
                     else
                             time = nowTime.hour + ":"+nowTime.minute;
 
+                    //
+                    Log.v("HEYHU", resultList.get(size).SSID + " " +resultList.get(size).level +resultList.get(size).BSSID);
+
+
+                    //
+
+
+
+
                     item.put("detectTime",time);
                     item.put("SSID", resultList.get(size).SSID); //SSID
                     item.put("power", resultList.get(size).level+"dBm"); //RSSI
                     item.put("MAC", resultList.get(size).BSSID); //MAC
-                    allWifiList.add(item);
+                    allWifiList.add(item); // 把所有的結果先放到list中 稍後排序後再把最強的取出
                     size--;
+                    //
                 }
+
+                Log.v("HEYHU", "All Wifi List size " + allWifiList.size());
 
                 Collections.sort(allWifiList, new Comparator<HashMap>() {
                     public int compare(HashMap lhs, HashMap rhs) {
@@ -135,11 +182,20 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+                for (int i = 0 ; i < temp; i++){
+
+                }
+
 
                 arrayList.add(allWifiList.get(0));
+
                 allWifiList.clear();
 
+
+
+                counter ++ ;
                 UI_handler.post(UI_display);
+                Log.v("HEYHU", "delayed.....");
                 mThreadHandler.postDelayed(this,5000);
 
             }catch(Exception e) {}
@@ -153,9 +209,8 @@ public class MainActivity extends AppCompatActivity {
 
             startBtn.setText("click!");
 
-            textStatus.setText("第" + arrayList.size() + "次掃描");
+            textStatus.setText("" + counter);
 
-            //更新listView內容
             adapter.notifyDataSetChanged();
         }
     };
@@ -181,17 +236,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
 
         super.onDestroy();
-        allWifiList.clear();
-        arrayList.clear();
+
+
+
         //移除工人上的工作
+
         if (mThreadHandler != null) {
+
             mThreadHandler.removeCallbacks(detectWifiLoop);
+
         }
 
+        //解聘工人 (關閉Thread)
+
         if (mThread != null) {
+
             mThread.quit();
+
         }
 
     }
 
+/*
+
+    @Override
+    public boolean onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main , menu);
+        return true;
+    }
+*/
 }
